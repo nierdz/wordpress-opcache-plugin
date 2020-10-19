69c3d89d3ca91b0f62048956a37c3efaf5645c94
nierdz <kevin@igln.fr>
2020-10-19 10:18:20 +0200

    feat: switch from travis to github actions

--- github-to-svn.sh
+++ github-to-svn.sh
@@ -6,6 +6,9 @@ set -o errexit
 DEBUG=${DEBUG:=0}
 [[ $DEBUG -eq 1 ]] && set -o xtrace
 
+SVN_REPOSITORY=https://plugins.svn.wordpress.org/flush-opcache
+SVN_USERNAME=mnttech
+
 # Checkout SVN repository
 svn co "$SVN_REPOSITORY" ./svn
 
@@ -24,7 +27,7 @@ svn add --force assets
 
 # Create SVN tag
 svn cp \
-  trunk "tags/$TRAVIS_TAG"
+  trunk "tags/${GITHUB_REF##*/}"
 
 #Remove missing files from svn
 missing_files=$(svn status | grep -E "^\!.*$" | awk '{print $2}')
@@ -34,7 +37,7 @@ done
 
 # Push to WordPress
 svn ci \
-  --message "Release $TRAVIS_TAG" \
+  --message "Release ${GITHUB_REF##*/}" \
   --username "$SVN_USERNAME" \
   --password "$SVN_PASSWORD" \
   --non-interactive
