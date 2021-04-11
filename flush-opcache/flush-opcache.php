06c7a08e3a551d90aeefd9f695c5ed7dbde17a55
nierdz <kevin@igln.fr>
2021-04-11 15:33:50 +0200

    chore: bump version up to 3.2.0

--- flush-opcache/flush-opcache.php
+++ flush-opcache/flush-opcache.php
@@ -22,7 +22,7 @@ if ( ! defined( 'WPINC' ) ) {
 	die;
 }
 
-define( 'FLUSH_OPCACHE_VERSION', '3.1.1' );
+define( 'FLUSH_OPCACHE_VERSION', '3.2.0' );
 define( 'FLUSH_OPCACHE_NAME', 'flush-opcache' );
 
 require plugin_dir_path( __FILE__ ) . 'includes/class-flush-opcache.php';
